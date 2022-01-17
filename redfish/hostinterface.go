f4948dae98071f3ba30ad7c7367e987430605119
Zainub Wahid <me.zainub@gmail.com>
2022-01-17 16:27:29 +0500

    Update hostinterface.go

--- redfish/hostinterface.go
+++ redfish/hostinterface.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 	"reflect"
 
-	"github.com/stmcginnis/gofish/common"
+	"github.com/ZainubW/gofish/common"
 )
 
 // AuthenticationMode is the method used for authentication.
