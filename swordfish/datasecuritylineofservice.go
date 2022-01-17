804bc967a38301a3925a6545ad6e353fd83f3179
Zainub Wahid <me.zainub@gmail.com>
2022-01-17 17:04:44 +0500

    Update datasecuritylineofservice.go

--- swordfish/datasecuritylineofservice.go
+++ swordfish/datasecuritylineofservice.go
@@ -7,7 +7,7 @@ package swordfish
 import (
 	"encoding/json"
 
-	"github.com/stmcginnis/gofish/common"
+	"github.com/ZainubW/gofish/common"
 )
 
 // DataSecurityLineOfService is used to describe data security service
