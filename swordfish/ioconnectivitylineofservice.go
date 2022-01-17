d8b68d5d8f3f3fb6014916b87aef0d540a3ab130
Zainub Wahid <me.zainub@gmail.com>
2022-01-17 17:08:19 +0500

    Update ioconnectivitylineofservice.go

--- swordfish/ioconnectivitylineofservice.go
+++ swordfish/ioconnectivitylineofservice.go
@@ -7,7 +7,7 @@ package swordfish
 import (
 	"encoding/json"
 
-	"github.com/stmcginnis/gofish/common"
+	"github.com/ZainubW/gofish/common"
 )
 
 // IOConnectivityLineOfService is used to specify the characteristics of
