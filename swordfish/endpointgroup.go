c2457d79f02df3019df662e696df6ed4c7246eb3
Zainub Wahid <me.zainub@gmail.com>
2022-01-17 17:06:18 +0500

    Update endpointgroup.go

--- swordfish/endpointgroup.go
+++ swordfish/endpointgroup.go
@@ -8,8 +8,8 @@ import (
 	"encoding/json"
 	"reflect"
 
-	"github.com/stmcginnis/gofish/common"
-	"github.com/stmcginnis/gofish/redfish"
+	"github.com/ZainubW/gofish/common"
+	"github.com/ZainubW/gofish/redfish"
 )
 
 // AccessState is used for associated resources through all
